a6d6126e81c36b0f261fe48e39ae7c5e3dd129f0
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-04 16:10:42 -0800

    Complete test cases for sum all

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,6 @@
-//1. Sub problem: Find a remove a single element given its index number
+
 const removeFromArray = function(array, ...args) {
   const resultArray = [];
-
-  //loop through all members of current list
-  //check on each element to see if it needs to be deleted (index or match)
-  //if it does not need to be deleted add it to the resultArray
-  //return resultArray
 
   array.forEach((item) => {
     if ( !args.includes(item) ) {
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,42 @@
-const sumAll = function() {
+
+
+    // error returned with negative numbers
+    // numbers can be in any order
+    // non-numbers are not acceptible
+function errorCheck(start, end) {
+    if(start < 0 || end < 0) {
+        throw new Error('ERROR');
+    }
+
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        throw new Error('ERROR');
+    }
+}
+
+const sumAll = function(start, end) {
+    let totalSum = 0;
+    let numArray = [];
+
+    if(start < 0 || end < 0) {
+        return 'ERROR';
+    }
+
+    if(!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
+   
+    if(start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for(let i=start; i <= end; i++) {
+        totalSum += i;
+    }
+
+
+    return totalSum;
 
 };
 
