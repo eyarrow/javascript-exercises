53904a7707044208b26247093591739861513918
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-04 16:42:27 -0800

    Complete testing on celsius to fahrenheit conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  if(Number.isInteger(celsius)) {
+    return celsius;
+  }
+  else {
+    let result = celsius.toFixed(1);
+    return Number.parseFloat(result);
+
+  }
+  
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * (9/5)) + 32;
+  if(Number.isInteger(fahrenheit)) {
+    return fahrenheit 
+  }
+  else {
+    let result = fahrenheit.toFixed(1);
+    return Number.parseFloat(result);
+  }
+
 };
 
 // Do not edit below this line
