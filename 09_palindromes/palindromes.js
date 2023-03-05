c9575edee28da2300a08146e48ba1f5856bb3252
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-04 17:18:30 -0800

    Adding palindrome reverse lookup

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let startIndex = 0;
+    let endIndex = string.length - 1;
+
+    let isPalindrome = false;
+
+    while (startIndex != endIndex) {
+        if(string[startIndex] == string[endIndex]) {
+            isPalindrome = true;
+            startIndex = startIndex + 1;
+            endIndex = endIndex - 1;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // 'racecar' length = 7
+    // start index = 0, endindex = 6
+    //
+
+    return isPalindrome;
 
 };
 
