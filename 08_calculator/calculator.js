e24e257cff89dcdfa21fcb53bf9102949a548018
Elizabeth Yarrow <eyarrow@outlook.com>
2023-03-04 16:51:09 -0800

    Finish add, subtract, sum, and multiply functionality

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,32 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let sum = 0;
+  
+  array.forEach((item) => {
+    sum += item;
+  })
+
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
+
+  array.forEach((item) => {
+    product *= item;
+  })
+
+  return product;
 
 };
 
