9f04d49f2e78248a8cae44bfb27b584eea37e2e6
Elizabeth Yarrow <eyarrow@outlook.com>
2023-04-05 13:21:57 -0700

    Update calculator functions to streamline

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,47 +1,22 @@
-const add = function(num1, num2) {
-  return num1 + num2;
-	
-};
+const add = ((num1, num2) => num1 + num2);
 
-const subtract = function(num1, num2) {
-  return num1 - num2;
-	
-};
+
+const subtract =((num1, num2) => num1 - num2);
 
 const sum = function(array) {
-  let sum = 0;
-  
-  array.forEach((item) => {
-    sum += item;
-  })
-
-  return sum;
-	
-};
+  return array.reduce((total, current) => total + current, 0);
+}
 
 const multiply = function(array) {
-  let product = 1;
-
-  array.forEach((item) => {
-    product *= item;
-  })
-
-  return product;
+  return array.reduce((accumulator, current) => accumulator * current);
 
 };
 
 const power = function(base, exponent) {
-  let raised = 1;
-
-  for(let i = 0; i < exponent; i++) {
-    raised *= base;
-  }
-
-  // 4 ^ 3 - 4, 16, 
-
-  return raised;
+  return Math.pow(base, exponent);
 	
 };
+
 
 const factorial = function(number) {
   let factorial = 1;
